d28cdeaefe335e3eca647ebecf26b6e737c750c0
Jake Garcia <bballwiz4@gmail.com>
2023-11-15 11:29:05 -0700

    Added a save button and gave it styling. No functionality yet.

--- src/components/ListCreationHeader/Forms/AddItems/index.js
+++ src/components/ListCreationHeader/Forms/AddItems/index.js
@@ -11,6 +11,7 @@ const AddItems = ({listName, list, newItemInfo, addItemClick, handleCompleteClic
             </div>
             <div className="w-1/2">
                 <StagedList handleCompleteClick={handleCompleteClick} listName={listName} list={list}/>
+                <button className="w-3/4 mx-auto bg-blue-500 mt-1 rounded-lg py-1 text-white font-bold transition-all duration-300 hover:bg-blue-700 hover:cursor-pointer">Save</button>
             </div>
         </div>
     )
