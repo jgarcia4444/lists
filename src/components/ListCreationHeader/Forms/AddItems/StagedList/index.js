667e5a4de1368d4d40ffeec01645be528829f151
Jake Garcia <bballwiz4@gmail.com>
2023-11-15 11:20:42 -0700

    Updated the Staged list styling. Gave a set height with y overflow set to auto. Styled the list items using their index to differentiate the opacity of their background.

--- src/components/ListCreationHeader/Forms/AddItems/StagedList/index.js
+++ src/components/ListCreationHeader/Forms/AddItems/StagedList/index.js
@@ -5,15 +5,19 @@ import ListItem from '../../../../../shared/lists/ListItem';
 
 const StagedList = ({listName, list, handleCompleteClick}) => {
     const renderItems = () => {
-        return list.map((itemInfo, i) => <ListItem handleCompleteClick={handleCompleteClick} itemInfo={itemInfo} key={`${i}-${itemInfo.item}`} />)
+        return list.map((itemInfo, i) => {
+            let adjustedItemInfo = itemInfo;
+            adjustedItemInfo["id"] = i;
+            return <ListItem handleCompleteClick={handleCompleteClick} itemInfo={adjustedItemInfo} key={`${i}-${itemInfo.item}`} />
+        } )
     }
 
     return (
-        <div className=" bg-blue-500 pb-2 rounded text-white">
-            <div className="bg-black bg-opacity-40 rounded-t py-1">
+        <div className=" mx-auto w-3/4 bg-blue-500 pb-1 rounded-lg text-white">
+            <div className="bg-black bg-opacity-40 rounded-t-lg py-1">
                 <FormInputLabel text={listName} />
             </div>
-            <div className="">
+            <div className="bg-black bg-opacity-10 h-60 m-2 mb-1 rounded-b-lg overflow-y-auto p-2 rounded-lg">
                 {renderItems()}
             </div>
         </div>
--- src/shared/lists/ListItem/index.js
+++ src/shared/lists/ListItem/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const ListItem = ({itemInfo, handleCompleteClick}) => {
-    const {item, complete} = itemInfo; 
+    const {item, complete, id} = itemInfo; 
     return (
-        <div className="flex flex-row mt-2">
+        <div className={`flex flex-row py-2 bg-black transition-all duration-300 mb-1 rounded-lg ${id % 2 === 0 ? "bg-opacity-10 hover:bg-opacity-20" : "bg-opacity-50 hover:bg-opacity-60"}`}>
             <div className="w-1/4 flex justify-center">
                 <div onClick={handleCompleteClick} className="w-6 h-6 rounded-full border-2 border-white flex items-center justify-center text-white hover:cursor-pointer hover:bg-white hover:bg-opacity-30 transition-all duration-300 hover:scale-105">
                     {complete === true ? "X" : ""}
