5f833dc10929bb0588da33504c6ac48125c33dea
Jake Garcia <bballwiz4@gmail.com>
2023-12-11 10:55:43 -0700

    Gave the cancel button on the item menu a margin right to space out the buttons.

--- src/shared/menus/ItemMenu.js
+++ src/shared/menus/ItemMenu.js
@@ -5,7 +5,7 @@ const ItemMenu = ({dismiss, destroy}) => {
 
     return (
         <div className="flex flex-row">
-            <FiX className='hover:cursor-pointer text-slate-600 hover:text-slate-300 transition-all duration-300' onClick={dismiss}  size={24} />
+            <FiX className='hover:cursor-pointer text-slate-200 hover:text-slate-400 transition-all duration-300 mr-2' onClick={dismiss}  size={24} />
             <FiTrash2 className='hover:cursor-pointer text-red-500 text-opacity-50 hover:text-opacity-100 transition-all duration-300' onClick={destroy} size={24} />
         </div>
     )
